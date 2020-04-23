336c58f2a7d78f7860a8a420de665b44173209d8
LM M <lm.meny28@gmail.com>
2020-04-23 10:43:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>  
+    <h1>{{ title + i+ times}}</h1>
+    <button @click="iAmClicked()">Click me!</button>
+    <button @click="resetInc()">Reset !</button>
+  </div>
+</template>
+ 
+<script>
+    export default {
+       
+        data() {
+            return {
+                title: 'you have cliked : ',
+                i : 0 ,
+                times :" times",
+            }
+        },
+        methods: {
+
+             resetInc(){
+                 this.title=" you have cliked : "; 
+                 this.i = 0 ;
+                 this.times =" times";
+            },
+        
+            iAmClicked() {
+                this.i +=1;
+                this.title = "you have cliked : ";
+                this.times =" times";
+            },
+
+           
+        }
+    };
+</script>
